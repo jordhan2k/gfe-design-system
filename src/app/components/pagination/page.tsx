ef362175b1aa1a044af2912fe77ada746c6a191a
dung.ht <dunghoang1c.hanu@gmail.com>
2025-08-10 18:07:11 +0700

    feat: add pagination component

--- src/app/components/pagination/page.tsx
+++ src/app/components/pagination/page.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from '@/components/ui/pagination';
 import { Metadata } from 'next';
 import React from 'react'
 
@@ -5,8 +6,28 @@ export const metadata: Metadata = {
   title: "Pagination component",
 };
 
+const paginationContainers = [
+  { id: 'pagination1', currentPage: 1, totalPages: 5, },
+  { id: 'pagination2', currentPage: 1, totalPages: 3, iconOnly: true },
+  { id: 'pagination3', currentPage: 1, totalPages: 10 },
+  { id: 'pagination4', currentPage: 5, totalPages: 10, iconOnly: true },
+];
+
+
 export default function PaginationPage() {
   return (
-    <div>PaginationPage</div>
-  )
+    <div className='py-[200px] mx-auto w-fit flex flex-col items-center gap-6 '>
+      {paginationContainers.map((pagination) => (
+        <div key={pagination.id}>
+          <Pagination
+            id={pagination.id}
+            initialPage={pagination.currentPage}
+            totalPages={pagination.totalPages}
+            iconOnly={pagination.iconOnly}
+          // onPageChange={(page) => console.log(`Page changed to: ${page}`)}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
