b940e2493d9f91e3d5e120197f8c530e419da4f4
jordhan2k <jordhan2k@users.noreply.github.com>
2025-08-11 10:49:04 +0000

    deploy: 8072d4931d597d3e4d6c8606e140d4c803b4dbec

---
+++ assets/badge.stories-DcKqRBQB.js
@@ -0,0 +1,31 @@
+import{j as p}from"./jsx-runtime-Db7AqdPk.js";import{c as u,a as l}from"./index-DFUPT9Yl.js";import"./iframe-Bj0Eugeg.js";import"./preload-helper-D9Z9MdNV.js";const b=l("text-sm rounded-full font-normal",{variants:{variant:{neutral:"text-neutral-600 bg-gray-50 border border-neutral-200",danger:"text-red-600 bg-red-50 border border-red-200",warning:"text-amber-700 bg-amber-50 border border-amber-200",success:"text-green-700 bg-green-50 border border-green-200",brand:"text-indigo-700 bg-indigo-50 border border-indigo-200"},size:{sm:"px-1.5 py-0.5 text-xs",md:"px-2 py-0.5",lg:"px-2.5 py-1"}},defaultVariants:{variant:"neutral",size:"sm"}});function o({children:d,className:c,variant:i,size:m,...g}){return p.jsx("span",{className:u(b({variant:i,size:m,className:c})),...g,children:d})}o.__docgenInfo={description:"",methods:[],displayName:"Badge"};const S={title:"ui/Badge",component:o,tags:["autodocs"],argTypes:{children:{control:"text"},variant:{control:"select",options:["neutral","danger","warning","success","brand"]},size:{control:"select",options:["sm","md","lg","xl","2xl"]}},args:{children:"Badge"},parameters:{layout:"centered"}},r={args:{variant:"brand",size:"md"}},e={args:{variant:"neutral",size:"md"}},a={args:{variant:"danger",size:"md"}},s={args:{variant:"warning",size:"md"}},n={args:{variant:"success",size:"md"}},t={args:{variant:"brand",size:"md"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'brand',
+    size: 'md'
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'neutral',
+    size: 'md'
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger',
+    size: 'md'
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'warning',
+    size: 'md'
+  }
+}`,...s.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'success',
+    size: 'md'
+  }
+}`,...n.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'brand',
+    size: 'md'
+  }
+}`,...t.parameters?.docs?.source}}};const y=["Default","Neutral","Danger","Warning","Success","Brand"];export{t as Brand,a as Danger,r as Default,e as Neutral,n as Success,s as Warning,y as __namedExportsOrder,S as default};
---
+++ assets/jsx-runtime-Db7AqdPk.js
@@ -0,0 +1,1 @@
+import{b as r}from"./iframe-Bj0Eugeg.js";var s=r();export{s as j};
